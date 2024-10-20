01a38c7672dd632106d25823f4bf75b44543b749
Shaxzod Ruziqulov <73300409+ShaxzodR@users.noreply.github.com>
2024-10-20 09:55:51 +0500

    commit

--- src/main/java/com/example/Hotel/managment/system/security/JwtService.java
+++ src/main/java/com/example/Hotel/managment/system/security/JwtService.java
@@ -76,15 +76,15 @@ public class JwtService {
 
     public boolean isTokenValid(String token, UserDetails userDetails) {
         final String username = extractUserName(token);
-        return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
+        return (username.equals(userDetails.getUsername())) && isTokenExpired(token);
     }
     public boolean isRefreshTokenValid(String token, UserDetails userDetails) {
         final String username = extractUserName(token);
-        return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
+        return (username.equals(userDetails.getUsername())) && isTokenExpired(token);
     }
 
     private boolean isTokenExpired(String token) {
-        return extractExpiration(token).before(new Date());
+        return !extractExpiration(token).before(new Date());
     }
 
     private Date extractExpiration(String token) {
